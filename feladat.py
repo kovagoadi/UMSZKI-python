9975fc17070e399e0c44dc5451288e74d315a135
Kővágó Ádám <kovagoadi@gmail.com>
2023-11-22 13:04:38 +0100

    Added new files

---
+++ feladat.py
@@ -0,0 +1,27 @@
+termek = ["Sertéskaraj", "Gouda sajt", "Dobozos sajt", "Csirkemell filé", "Mirelit Pizza", "Vaj", "Edámi sajt", "Szőlő", "Paradicsom", "Szalámi", "Rizs", "Banán", "Zöldségmix", "Cola", "Hagyma", "Kenyér", "Narancs", "Alma", "Napraforgóolaj", "Spaghetti tészta", "Teljes tej", "Joghurt", "Almalé", "Burgonya", "Babkonzerv", "Cukor", "WC-papír", "Búzaliszt", "Friss tojás"]
+egyseg = ["kg", "450 g", "400 g", "kg", "db", "250 g", "200 g", "1 kg", "1 kg", "100 g", "1 kg", "1 kg", "1 kg", "2l", "1 kg", "1 kg", "1 kg", "1 kg", "1 l", "500 g", "1 l", "500 ml", "1 l", "1 kg", "240 g", "1 kg", "tekercs", "1 kg", "db"]
+
+# 1. feladat
+# ▪ Kérje be a felhasználótól egy keresett termék nevét!
+# ▪ Ellenőrizze, hogy az megtalálható-e a termékek között!
+# ▪     Ha igen, akkor állapítsa meg az indexét és írja ki a kiszerelés egységét!
+# ▪     Ha nem, akkor a parancssoron a „Nincs ilyen termék” üzenet jelenjen meg!
+
+december = [ 2550, 1602, 1140, 1620, 1320, 682, 750, 1700, 1000, 693, 651, 686, 805, 702, 600, 582, 798, 648, 522, 605, 550, 567, 286, 350, 351, 250, 154, 135, 50 ]
+november = [ 2400, 1800, 1500, 1500, 1500, 1100, 1000, 1000, 1000, 770, 700, 700, 700, 600, 600, 600, 600, 600, 580, 550, 440, 540, 440, 350, 270, 250, 220, 150, 80 ]
+
+# 2. feladat
+# ▪ Vezessen be egy üres listát 'változás' azonosítóval!
+# ▪ Vezessen be egy index változót, amivel az aktuális szám sorrendjét számolja
+# ▪ Egy megfelelő ciklus segítségével járja végig egyszerre a két listát!
+#     ▪ Számítsa ki a két értékpár százalékát az alábbi képlet segítségével:
+#     ▪ 'százalék' legyen (decemberi / novemberi - 1) * 100
+#     ▪ Kerekítse két tizedesjegyre a 'round(százalék, 2)' függvény használatával!
+#     ▪ Füzze az eredményt a 'változás' lista végéhez!
+# ▪ A ciklus után írja képernyőre a kiszámolt adatokat!
+
+
+# 3. feladat
+# ▪ importálja a json modult!
+# ▪ készítsen az adatokból szótárat
+# ▪ mentse fájlba data.json néven!
---
+++ readme.md
@@ -0,0 +1,28 @@
+# Git
+
+## GitHub
+- hozzon létre egy új remote repositoryt
+- a neve legyen UMSZKI-python
+- láthatóságát hagyja nyilvánosan
+- ne tartalmazzon alapértelmezett readme állományt
+- a .gitignore sablonjának válassza a Visual Studiot
+
+## VS Code
+- nyisson egy új terminált
+- konfigurálja a git felhasználónevét és email címét
+- klónozza a projectet az Asztalra
+- módosítsa a .gitignore állományt úgy
+- hogy .vs/ mappa ne szinkronizálódjon
+- készítsen commitot "setup" üzenettel
+- pusholja a remote repositoryba
+
+## Branching
+- nyisson új branchet a main alapján ora-1122 néven
+- helyezze el benne ezt az állományt és a feladat.py fájlt
+- oldja meg a feladatokat
+- minden feladat végén commitoljon
+
+## Merging
+- váltson a "main" ágra
+- merge-elje bele a "ora-1122" ágat
+- pusholja a megoldását a remote repositoryba
